d874b5a7c16ce862e5c424a8e08554d4e6857ad5
Benedettopana <benedetto.pana@gmail.com>
2024-05-13 15:03:45 +0200

    [add]train migration

---
+++ database/migrations/2024_05_13_124534_create_train_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('train_migration', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency',25);
+            $table->string('departure_station',50);
+            $table->string('arrival_station',50);
+            $table->time('departure_time')->format('H,i,s');
+            $table->time('arrival_time')->format('H,i,s');
+            $table->string('train_code',25);
+            $table->tinyInteger('carriages_number')->unsigned();
+            $table->boolean('in_time')->default(1);
+            $table->boolean('cancelled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('train_migration');
+    }
+};
